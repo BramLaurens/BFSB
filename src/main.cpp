db530e711e680a1f78cfbbaee2488677455a1a0e
isisvangestel <isisvangestel300@gmail.com>
2024-10-15 20:34:06 +0200

    wijzigingen display

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,17 @@
-// #include <Arduino.h>
-// #include <DabbleESP32.h>
-
-// // put function declarations here:
-
-// #define motorL_FWD 16
-// #define motorL_REV 0
-// #define motorR_FWD 2
-// #define motorR_REV 15
-
-// #define motorPWMres 8
-// #define motorPWMfreq 200
-
-// void setup() {
-//   // put your setup code here, to run once:
-//   Serial.begin(115200);
-//   //Dabble.begin("ESP32_Bram");
-// }
-// void loop() {
-
-// }
-
-// //-------------------------------------------
-#include <Arduino.h>
-#include <DabbleESP32.h>
-
-
 /*********
   Rui Santos
   Complete project details at https://randomnerdtutorials.com  
 *********/
 
+#include <Arduino.h>
+#include <DabbleESP32.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// #define BLUE 0x03
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
@@ -54,11 +30,18 @@ void setup() {
 }
 
 void loop() {
-  display.setTextSize(1);
+  //display.setRotation(1); //Voer een waarde tussen 1 en 3 in of comment deze line; 1 draait het beeld met 90 graden, 2 draait het beeld met 180 graden, 3 draait het beeld 
+  display.setTextSize(5);
+  //Grootte tekst in pixels (max 6)
   display.setTextColor(WHITE);
-  display.setCursor(0, 10);
+  //
+  display.setCursor(50, 30); // int x, int y; waardes om in te stellen hoe ver opzij of hoe hoog de invoer wordt uitgebeeld. (Hoe hoger de y, en hoe hoger de x, )
   // Display static text
-  display.println("Finn, jij bent een");
-  display.println("piemol!!! >:(");
+  display.println("T"); //invoer wat wordt uitgebeeld op display
   display.display(); 
 }
+
+
+
+//int optellen(int a, int b) {
+  //return a + b; // Retourneert de som van a en b
