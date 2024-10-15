9d9ecb574489ba93f7c144d71c50bed73c321ffb
Matthias Dercksen <matthias.dercksen@gmail.com>
2024-10-15 13:22:12 +0200

    Testprogramma voor SR_04 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,49 @@
-/*
-Testcode voor de CNY70 sensor
-VCC = 5V
-Weerstand = 330 ohm
-*/
+// #include <Arduino.h>
+// #include <DabbleESP32.h>
+
+// void setup() {
+//   Serial.begin(9600);
+//   Serial.println("Begin");
+// }
+
+// void loop() {
+
+// }
 
 #include <Arduino.h>
 #include <DabbleESP32.h>
-#define Drempelwaarde 500
-//Test
+
+#define TRIG_PIN 23 // ESP32 pin GPIO23 connected to Ultrasonic Sensor's TRIG pin
+#define ECHO_PIN 22 // ESP32 pin GPIO22 connected to Ultrasonic Sensor's ECHO pin
+
+float duration_us, distance_cm;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Begin");
-  pinMode(34, INPUT);
+  // begin serial port
+  Serial.begin (115200);
+
+  // configure the trigger pin to output mode
+  pinMode(TRIG_PIN, OUTPUT);
+  // configure the echo pin to input mode
+  pinMode(ECHO_PIN, INPUT);
 }
 
 void loop() {
-  // if (analogRead(34) > Drempelwaarde)
-  // {
-  //   Serial.println("Licht");
-  // } else
-  // {
-  //   Serial.println("Donker");
-  // }
-  Serial.println(analogRead(34));
-}
+  // generate 10-microsecond pulse to TRIG pin
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
 
+  // measure duration of pulse from ECHO pin
+  duration_us = pulseIn(ECHO_PIN, HIGH);
+
+  // calculate the distance
+  distance_cm = 0.017 * duration_us;
+
+  // print the value to Serial Monitor
+  Serial.print("distance: ");
+  Serial.print(distance_cm);
+  Serial.println(" cm");
+
+  delay(500);
+}
