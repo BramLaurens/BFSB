9c15f18548b2907c86640655b6584e1414adf0fc
isisvangestel <isisvangestel300@gmail.com>
2024-10-15 13:25:40 +0200

    added libraries for OLed

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,64 @@
+// #include <Arduino.h>
+// #include <DabbleESP32.h>
+
+// // put function declarations here:
+
+// #define motorL_FWD 16
+// #define motorL_REV 0
+// #define motorR_FWD 2
+// #define motorR_REV 15
+
+// #define motorPWMres 8
+// #define motorPWMfreq 200
+
+// void setup() {
+//   // put your setup code here, to run once:
+//   Serial.begin(115200);
+//   //Dabble.begin("ESP32_Bram");
+// }
+// void loop() {
+
+// }
+
+// //-------------------------------------------
 #include <Arduino.h>
 #include <DabbleESP32.h>
 
-// put function declarations here:
 
-#define motorL_FWD 16
-#define motorL_REV 0
-#define motorR_FWD 2
-#define motorR_REV 15
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
 
-#define motorPWMres 8
-#define motorPWMfreq 200
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  //Dabble.begin("ESP32_Bram");
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  
 }
+
 void loop() {
-
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Finn, jij bent een");
+  display.println("piemol!!! >:(");
+  display.display(); 
 }
-
