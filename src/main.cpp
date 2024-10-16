062854df86cbe28625c04148fbe2ab265d269e67
isisvangestel <isisvangestel300@gmail.com>
2024-10-16 14:16:09 +0200

    Opzet voor functie display

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ void setup() {
 }
 
 void loop() {
-  //display.setRotation(1); //Voer een waarde tussen 1 en 3 in of comment deze line; 1 draait het beeld met 90 graden, 2 draait het beeld met 180 graden, 3 draait het beeld 
+  display.setRotation(2); //Voer een waarde tussen 1 en 3 in of comment deze line; 1 draait het beeld met 90 graden, 2 draait het beeld met 180 graden, 3 draait het beeld 
   display.setTextSize(5);
   //Grootte tekst in pixels (max 6)
   display.setTextColor(WHITE);
@@ -43,5 +43,16 @@ void loop() {
 
 
 
-//int optellen(int a, int b) {
-  //return a + b; // Retourneert de som van a en b
+//int Display(InvoerDisplay) {
+
+  //return a + b; // Retourneert de som van a en b}
+  /*   display.setRotation(2); //Voer een waarde tussen 1 en 3 in of comment deze line; 1 draait het beeld met 90 graden, 2 draait het beeld met 180 graden, 3 draait het beeld 
+  display.setTextSize(5);
+  //Grootte tekst in pixels (max 6)
+  display.setTextColor(WHITE);
+  //
+  display.setCursor(50, 30); // int x, int y; waardes om in te stellen hoe ver opzij of hoe hoog de invoer wordt uitgebeeld. (Hoe hoger de y, en hoe hoger de x, )
+  // Display static text
+  display.println("T"); //invoer wat wordt uitgebeeld op display
+  display.display(); 
+  */
