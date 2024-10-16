08cc830a108e8b6692c38dbe29cb870557d20313
DaanBroekkie <daanfransbroekhuizen@gmail.com>
2024-10-16 13:54:51 +0200

    Removed deprecated code

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,14 @@
 #include <Arduino.h>
-#include <DabbleESP32.h>
-
+#include <RemoteXY.h>
 // put function declarations here:
 
-#define motorL_FWD 16
-#define motorL_REV 0
-#define motorR_FWD 2
-#define motorR_REV 15
-
-#define motorPWMres 8
-#define motorPWMfreq 200
-
-void forward(void);
-void brake(void);
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  //Dabble.begin("ESP32_Bram");
-
-  ledcAttachPin(motorL_FWD, 0);
-  ledcSetup(0, motorPWMfreq, motorPWMres);
+  
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-  /*
-  Dabble.processInput();
-  
-  if(GamePad.isCirclePressed()){
-    forward();
-  }
-  */
-  ledcWrite(motorL_FWD, 100);
-  Serial.println("Test");
 }
 
-void forward(void){
-  analogWrite(motorL_FWD, 100);
-  digitalWrite(motorL_REV, LOW);
-  analogWrite(motorR_FWD, 100);
-  digitalWrite(motorR_REV, LOW);
-}
-
-void brake(void){
-  analogWrite(motorL_FWD, 0);
-  digitalWrite(motorL_REV, LOW);
-  analogWrite(motorR_FWD, 0);
-  digitalWrite(motorR_REV, LOW);
-}
