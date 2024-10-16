b8583e907c439a69c628d832c32f8b3ecc560d8f
Matthias Dercksen <matthias.dercksen@gmail.com>
2024-10-16 14:24:16 +0200

    Motor test debug

--- src/main.cpp
+++ src/main.cpp
@@ -36,23 +36,32 @@ void setup() {
 }
 
 void loop() {
+  digitalWrite(motorL_FWD, LOW);
+  digitalWrite(motorR_REV, LOW);
   ledcWrite(ch_motorL_FWD, 100);
-  digitalWrite(ch_motorL_REV, LOW);
   ledcWrite(ch_motorR_FWD, 100);
-  digitalWrite(ch_motorR_REV, LOW);
   Serial.println("Voor");
   delay(500);
+
   ledcWrite(ch_motorL_FWD, 0);
-  digitalWrite(ch_motorL_REV, LOW);
+  ledcWrite(ch_motorL_REV, 0);
   ledcWrite(ch_motorR_FWD, 0);
-  digitalWrite(ch_motorR_REV, LOW);
+  ledcWrite(ch_motorR_REV, 0);
   Serial.println("Stop");
   delay(500);
-  ledcWrite(ch_motorL_FWD, 100);
-  digitalWrite(ch_motorL_REV, HIGH);
-  ledcWrite(ch_motorR_FWD, 100);
-  digitalWrite(ch_motorR_REV, HIGH);
+
+  digitalWrite(motorL_FWD, LOW);
+  digitalWrite(motorR_REV, LOW);
+  ledcWrite(ch_motorL_REV, 100);
+  ledcWrite(ch_motorR_REV, 100);
   Serial.println("Achter");
+  delay(500);
+
+  ledcWrite(ch_motorL_FWD, 0);
+  ledcWrite(ch_motorL_REV, 0);
+  ledcWrite(ch_motorR_FWD, 0);
+  ledcWrite(ch_motorR_REV, 0);
+  Serial.println("Stop");
   delay(500);
 }
 
