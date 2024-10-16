2a62a35fb120ff459fae507f43d52c7a02be4b70
Matthias Dercksen <matthias.dercksen@gmail.com>
2024-10-16 15:19:23 +0200

    Microswitch toegevoegd aan de testcode

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,32 @@
 #include <Arduino.h>
-#include <DabbleESP32.h>
 
-// put function declarations here:
+void Microswitch();
 
-#define motorL_FWD 4
-#define motorL_REV 0
-#define motorR_FWD 2
-#define motorR_REV 15
-
-#define ch_motorL_FWD 0
-#define ch_motorL_REV 1
-#define ch_motorR_FWD 2
-#define ch_motorR_REV 3
-
-#define motorPWMres 8
-#define motorPWMfreq 200
-
-void setup() {
-  digitalWrite(ch_motorL_REV, LOW);
-  digitalWrite(ch_motorR_REV, LOW);
-  digitalWrite(ch_motorL_FWD, LOW);
-  digitalWrite(ch_motorR_FWD, LOW);
-
-  Serial.begin(115200);
-
-  ledcAttachPin(motorL_FWD, 0);
-  ledcAttachPin(motorL_REV, 1);
-  ledcAttachPin(motorR_FWD, 2);
-  ledcAttachPin(motorR_REV, 3);
-
-  ledcSetup(0, motorPWMfreq, motorPWMres);
-  ledcSetup(1, motorPWMfreq, motorPWMres);
-  ledcSetup(2, motorPWMfreq, motorPWMres);
-  ledcSetup(3, motorPWMfreq, motorPWMres);
+void setup(){
+ Serial.begin(115200);
+ Serial.println("Testing all components \nPlease wait:");
+ pinMode(23, INPUT_PULLUP);
 }
 
-void loop() {
-  ledcWrite(ch_motorL_FWD, 100);
-  digitalWrite(ch_motorL_REV, LOW);
-  ledcWrite(ch_motorR_FWD, 100);
-  digitalWrite(ch_motorR_REV, LOW);
-  Serial.println("Voor");
-  delay(500);
-  ledcWrite(ch_motorL_FWD, 0);
-  digitalWrite(ch_motorL_REV, LOW);
-  ledcWrite(ch_motorR_FWD, 0);
-  digitalWrite(ch_motorR_REV, LOW);
-  Serial.println("Stop");
-  delay(500);
-  ledcWrite(ch_motorL_FWD, 100);
-  digitalWrite(ch_motorL_REV, HIGH);
-  ledcWrite(ch_motorR_FWD, 100);
-  digitalWrite(ch_motorR_REV, HIGH);
-  Serial.println("Achter");
-  delay(500);
+void loop(){
+  Microswitch();
+  delay(7000);
 }
 
+void Microswitch(){
+  Serial.println("Testing microswitch (normal open):\n");
+  delay(500);
+  Serial.println("Stap 1: \nSluit pin 1 van de microswitch aan op pin 23 van de ESP\n");
+  delay(500);
+  Serial.println("Stap 2: \nSluit pin 2 van de microswitch aan op een 10K resistor en de resistor op GND\n");
+  delay(500);
+  Serial.println("Stap 3: \nAls je alles hebt aangesloten mag je de knop indrukken als je er klaar voor bent:");
+
+  while (digitalRead(23)){}
+  Serial.println("Als je de knop nu hebt ingedrukt is de test geslaagd");
+  delay(500);
+  Serial.println("Als je de knop niet in hebt gedrukt is de test gefaald");
+  delay(500);
+  Serial.println("Dit was de microswitch test");
+  while (digitalRead(23) == true){}  
+}
