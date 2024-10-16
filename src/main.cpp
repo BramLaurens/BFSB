cd629d6113944c212091502c7d8a5cbf344870e0
bramlaurens <bramlaurens@gmail.com>
2024-10-16 22:29:16 +0200

    Fixed RemoteXY compatibility

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,63 @@
 #include <WiFi.h>
 #include <RemoteXY.h>
 
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// you can enable debug logging to Serial at 115200
+//#define REMOTEXY__DEBUGLOG    
+
+// RemoteXY GUI configuration  
+#pragma pack(push, 1)  
+uint8_t RemoteXY_CONF[] =   // 84 bytes
+  { 255,5,0,0,0,77,0,19,0,0,0,0,31,2,106,200,200,84,1,1,
+  4,0,1,58,64,57,57,154,7,24,24,2,137,31,70,87,68,0,1,55,
+  112,57,57,154,46,24,24,2,2,31,82,69,86,0,5,208,17,143,143,14,
+  7,60,60,0,2,26,31,1,37,72,57,57,118,27,24,24,1,37,31,66,
+  79,78,75,0 };
+  
+// this structure defines all the variables and events of the control interface 
+struct {
+
+    // input variables
+  uint8_t button_01; // =1 if button pressed, else =0
+  uint8_t button_02; // =1 if button pressed, else =0
+  int8_t joystick_01_x; // from -100 to 100
+  int8_t joystick_01_y; // from -100 to 100
+  uint8_t button_03; // =1 if button pressed, else =0
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0
+
+} RemoteXY;   
+#pragma pack(pop)
+ 
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
 #define TRIG_PIN 23 // ESP32 pin GPIO23 connected to Ultrasonic Sensor's TRIG pin
 #define ECHO_PIN 22 // ESP32 pin GPIO22 connected to Ultrasonic Sensor's ECHO pin
 
 float duration_us, distance_cm;
 
+/*Aanmkaen van een remotexy object/class*/
+CRemoteXY *remotexy;
+
 void setup() {
+  /*Initializering van het RemoteXY netwerk*/
+  remotexy = new CRemoteXY (
+    RemoteXY_CONF_PROGMEM, 
+    &RemoteXY, 
+    new CRemoteXYConnectionServer (
+      new CRemoteXYComm_WiFiPoint (
+        "BFSB_ESP32_Bram",       // REMOTEXY_WIFI_SSID
+        "12345678"),        // REMOTEXY_WIFI_PASSWORD
+      6377                  // REMOTEXY_SERVER_PORT
+    )
+  );  
+
   // begin serial port
   Serial.begin (115200);
 
@@ -18,6 +69,7 @@ void setup() {
 }
 
 void loop() {
+  remotexy->handler ();
   // generate 10-microsecond pulse to TRIG pin
   digitalWrite(TRIG_PIN, HIGH);
   RemoteXY_delay(1);
