4c775eb491981e81fc3d3e115f2ec6d49437cd44
DaanBroekkie <daanfransbroekhuizen@gmail.com>
2024-10-17 14:45:36 +0200

    Ultrasoon met blocking code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
 #include <Arduino.h>
 #include <RemoteXY.h>
-// put function declarations here:
-
+const int trigPin = 13;
+const int echoPin = 12;
+int duration;
+int distance;
 
 void setup() {
-  
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(115200); // Starts the serial communication
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, LOW);
+  delay(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delay(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPi, returns the sound wave travel time in microseconds
+  duration = pulseInLong(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
 }
 
