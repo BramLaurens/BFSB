e1305b3a9c800ef9e92cd22a3b08e25b7e4d14b7
Matthias Dercksen <matthias.dercksen@gmail.com>
2024-10-17 16:49:54 +0200

    Beeldscherm gefixt

--- src/main.cpp
+++ src/main.cpp
@@ -273,11 +273,17 @@ void Display(int InvoerDisplay){
 
   for (int i = 1; i <= 10; i++)
   {
+    display.clearDisplay();
     //display.setRotation(1); //Voer een waarde tussen 1 en 3 in of comment deze line; 1 draait het beeld met 90 graden, 2 draait het beeld met 180 graden, 3 draait het beeld 
-    display.setTextSize(5);
+    display.setTextSize(9);
     //Grootte tekst in pixels (max 6)
+    if (i==10){
+      display.setCursor(10, 0);// int x, int y; waardes om in te stellen hoe ver opzij of hoe hoog de invoer wordt uitgebeeld. (Hoe hoger de y, en hoe hoger de x, )
+    } else {
+      display.setCursor(40, 0);// int x, int y; waardes om in te stellen hoe ver opzij of hoe hoog de invoer wordt uitgebeeld. (Hoe hoger de y, en hoe hoger de x, )
+    }
+
     display.setTextColor(WHITE);//De kleur kan je als het goed is niet veranderen
-    display.setCursor(50, 30); // int x, int y; waardes om in te stellen hoe ver opzij of hoe hoog de invoer wordt uitgebeeld. (Hoe hoger de y, en hoe hoger de x, )
     // Display static text
     display.println(InvoerDisplay+i); //invoer wat wordt uitgebeeld op display
     display.display(); 
