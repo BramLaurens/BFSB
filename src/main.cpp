e60df14aa0b2cd040928c19d3755530900d276f5
Matthias Dercksen <matthias.dercksen@gmail.com>
2024-10-18 00:48:19 +0200

    Inplementatie commentaar

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,47 @@
+/*
+Voor integreren in main code:
+
+//////////////////////////////////////
+Gedefinieerde waardes:
+//////////////////////////////////////
+
+#define Drempelwaarde_CNY70 2000      //Drempelwaarde voor wit/zwart
+
+
+
+//////////////////////////////////////
+Prototypes:
+//////////////////////////////////////
+
+int CNY70();
+
+
+
+///////////////////////////////////////
+In de void setup():
+///////////////////////////////////////
+
+pinMode(34, INPUT);                    //Pin voor de CNY70
+
+
+
+////////////////////////////////////////
+Microswitch functie:
+////////////////////////////////////////
+
+int CNY70(){
+  if (analogRead(34) >= Drempelwaarde_CNY70){
+    return(0);
+    // Serial.println("Zwart");
+  }
+
+  if (analogRead(34) <= Drempelwaarde_CNY70){
+    // Serial.println("Wit");
+    return(1);
+  }
+}
+*/
+
 #include <Arduino.h>
 #include <Wifi.h>
 #include <RemoteXY.h>
