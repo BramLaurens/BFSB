b114bc1cf57c27db21465af2a3b74f933aade20e
Matthias Dercksen <matthias.dercksen@gmail.com>
2024-10-18 00:32:55 +0200

    Display improvement

--- src/main.cpp
+++ src/main.cpp
@@ -118,6 +118,9 @@ void setup(){
   }
   delay(2000);
   display.clearDisplay();
+  //display.setRotation(1); //Voer een waarde tussen 1 en 3 in of comment deze line; 1 draait het beeld met 90 graden, 2 draait het beeld met 180 graden, 3 draait het beeld met 270 graden
+  display.setTextSize(9);
+  display.setTextColor(WHITE);//De kleur kan je als het goed is niet veranderen
 
   digitalWrite(ch_motorL_REV, LOW);
   digitalWrite(ch_motorR_REV, LOW);
@@ -237,12 +240,11 @@ void reverse(){
 }
 
 void Display(int InvoerDisplay) {
-  //display.setRotation(1); //Voer een waarde tussen 1 en 3 in of comment deze line; 1 draait het beeld met 90 graden, 2 draait het beeld met 180 graden, 3 draait het beeld 
-  display.setTextSize(5);
-  //Grootte tekst in pixels (max 6)
-  display.setTextColor(WHITE);//De kleur kan je als het goed is niet veranderen
-  display.setCursor(50, 30); // int x, int y; waardes om in te stellen hoe ver opzij of hoe hoog de invoer wordt uitgebeeld. (Hoe hoger de y, en hoe hoger de x, )
-  // Display static text
+  if (InvoerDisplay >= 10){
+    display.setCursor(10, 0);
+  } else {
+    display.setCursor(40, 0);
+  }
   display.println(InvoerDisplay); //invoer wat wordt uitgebeeld op display
   display.display(); 
 }
