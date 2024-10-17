0804421c789dbd5e771fa153de967d043e883e18
isisvangestel <isisvangestel300@gmail.com>
2024-10-17 14:14:46 +0200

    Complete functie display

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+void Display(int InvoerDisplay);
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 // #define BLUE 0x03
@@ -30,29 +32,27 @@ void setup() {
 }
 
 void loop() {
-  display.setRotation(2); //Voer een waarde tussen 1 en 3 in of comment deze line; 1 draait het beeld met 90 graden, 2 draait het beeld met 180 graden, 3 draait het beeld 
-  display.setTextSize(5);
-  //Grootte tekst in pixels (max 6)
-  display.setTextColor(WHITE);
-  //
-  display.setCursor(50, 30); // int x, int y; waardes om in te stellen hoe ver opzij of hoe hoog de invoer wordt uitgebeeld. (Hoe hoger de y, en hoe hoger de x, )
-  // Display static text
-  display.println("T"); //invoer wat wordt uitgebeeld op display
-  display.display(); 
+  Display(60); 
+  // display.setRotation(2); //Voer een waarde tussen 1 en 3 in of comment deze line; 1 draait het beeld met 90 graden, 2 draait het beeld met 180 graden, 3 draait het beeld 
+  // display.setTextSize(5);
+  // //Grootte tekst in pixels (max 6)
+  // display.setTextColor(WHITE);
+  // //
+  // display.setCursor(50, 30); // int x, int y; waardes om in te stellen hoe ver opzij of hoe hoog de invoer wordt uitgebeeld. (Hoe hoger de y, en hoe hoger de x, )
+  // // Display static text
+  // display.println("T"); //invoer wat wordt uitgebeeld op display
+  // display.display(); 
 }
 
 
-
-//int Display(InvoerDisplay) {
-
-  //return a + b; // Retourneert de som van a en b}
-  /*   display.setRotation(2); //Voer een waarde tussen 1 en 3 in of comment deze line; 1 draait het beeld met 90 graden, 2 draait het beeld met 180 graden, 3 draait het beeld 
+//functie displaytekst
+void Display(int InvoerDisplay) {
+  //display.setRotation(1); //Voer een waarde tussen 1 en 3 in of comment deze line; 1 draait het beeld met 90 graden, 2 draait het beeld met 180 graden, 3 draait het beeld 
   display.setTextSize(5);
   //Grootte tekst in pixels (max 6)
-  display.setTextColor(WHITE);
-  //
+  display.setTextColor(WHITE);//De kleur kan je als het goed is niet veranderen
   display.setCursor(50, 30); // int x, int y; waardes om in te stellen hoe ver opzij of hoe hoog de invoer wordt uitgebeeld. (Hoe hoger de y, en hoe hoger de x, )
   // Display static text
-  display.println("T"); //invoer wat wordt uitgebeeld op display
+  display.println(InvoerDisplay); //invoer wat wordt uitgebeeld op display
   display.display(); 
-  */
+}
