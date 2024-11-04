f7d8cfb7dba22ea1e9ca7fe0ca17f6f569ac94e8
bramlaurens <bramlaurens@gmail.com>
2024-11-04 13:32:32 +0100

    Changed SSID

--- src/main.cpp
+++ src/main.cpp
@@ -195,7 +195,7 @@ void setup(){
     &RemoteXY, 
     new CRemoteXYConnectionServer (
       new CRemoteXYComm_WiFiPoint (
-        "BFSB_ESP32_Daan",       // REMOTEXY_WIFI_SSID
+        "BFSB_ESP32_Bram",       // REMOTEXY_WIFI_SSID
         "12345678"),        // REMOTEXY_WIFI_PASSWORD
       6377                  // REMOTEXY_SERVER_PORT
     )
