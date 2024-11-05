1fb8a47a2abeb66f9bc1f20e98749c6565645bec
bramlaurens <bramlaurens@gmail.com>
2024-11-05 21:04:44 +0100

    Activated arena func & changed Espnow refresh rate

--- src/main.cpp
+++ src/main.cpp
@@ -226,7 +226,7 @@ bool SOGroutineDone = false;
 CRemoteXY *remotexy;
 
 void sendScore(){
-  if(millis() - ESPnowTimer > 1000){
+  if(millis() - ESPnowTimer > 1006){
     encodedScore[1] = abs(Score);
 
     if(Score < 0){
@@ -361,7 +361,7 @@ void loop() {
     remoteMotorcontrol();
     servo();
     microswitch();
-    //arena_border();
+    arena_border();
     if(Score != lastScore){
       Display(Score);
       lastScore = Score;
